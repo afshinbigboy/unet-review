f158453a6e60871a45c922268292085a230cf0b1
Afshin Bozorgpour <afshinbigboy@gmail.com>
2022-04-28 23:09:35 +0430

    init

--- src/datasets.py
+++ src/datasets.py
@@ -1,20 +1,18 @@
+from __future__ import print_function, division
+
+import os
+import pandas as pd
+import numpy as np
+import glob
+from skimage import io, transform
+import matplotlib.pyplot as plt
 import torch
 from torch.utils.data.dataset import Dataset  # For custom data-sets
 from torchvision import transforms
-from PIL import Image
-import glob
-
-
-
-from __future__ import print_function, division
-import os
-import torch
-import pandas as pd
-from skimage import io, transform
-import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+from matplotlib.image import imread
+
 
 # Ignore warnings
 import warnings
@@ -23,55 +21,62 @@ warnings.filterwarnings("ignore")
 plt.ion()   # interactive mode
 
 
+
 class ISIC2018Dataset(Dataset):
-    """ISIC 2018 Dataset."""
-    def __init__(self, root_dir, mode, transform=None, *args, **kwargs):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        if mode.lower() not in ['train', 'validation', 'test']:
-            raise ValueError("Invalid mode! Valid modes are: ['train', 'validation', 'test']")
-        
-        if mode.lower() == "train":
-            pass
-        if mode.lower() == "validation":
-            pass
-        if mode.lower() == "test":
-            pass
-        
-        subject_names = ['-'.join(fn.split('-')[1:]) for fn in subjects]
-        subjects_x = subjects
-        subjects_y = subjects
-        
-        self.X_path_list = subjects_x
-        self.Y_path_list = subjects_y
-        
-        self.output_size = output_size
-        
-    def __len__(self):
-        return len(self.X_path_list)
-    
-    def __getitem__(self, index):
-        x = Image.open(self.X_path_list[index])
-        y = Image.open(self.Y_path_list[index])
-        filename = self.file_names[index]
-        
-        x = torch.tensor(x).permute(2,0,1)
-        if len(y.shape) < 3: 
-            m = torch.tensor(y).unsqueeze(-1).permute(2,0,1)
-        else: 
-            m = torch.tensor(y).permute(2,0,1)
-        
-        # x and y must resize to the self.output_size
-        sample = {'image': x, 'mask': m}
-        if self.transform:
-            sample = self.transform(sample)
+	"""ISIC 2018 Dataset."""
+	def __init__(
+		self, 
+		root_dir, 
+		x_folder,
+		x_filename_format,
+		y_folder=None,
+		y_filename_format=None,
+		transform_list=[], 
+		*args, **kwargs):
+		"""Args:
+			csv_file (string): Path to the csv file with annotations.
+			root_dir (string): Directory with all the images.
+			transform (callable, optional): Optional transform to be applied
+				on a sample.
+		"""
+		self.root_dir = root_dir if not '/' == root_dir[-1] else os.path.dirname(root_dir)
+		self.x_fp_list = glob.glob(f"{os.path.join(self.root_dir, x_folder)}/{x_filename_format}")
+		self.fid_list = [fn.split('/')[-1].split('.')[0].split('_')[1] for fn in self.x_fp_list]
+		
+		self.x_folder = x_folder
+		self.x_filename_format = x_filename_format
+		self.y_folder = y_folder
+		self.y_filename_format = y_filename_format
 
-        return sample
+		if transform_list:
+			self.transform = transforms.Compose([eval(t) for t in transform_list])
+		else:
+			self.transform = None
+		
 
-    def __len__(self):
-        return len(self.file_names)
+	def __len__(self):
+		return len(self.fid_list)
+
+
+	def __getitem__(self, index):
+		file_id = self.fid_list[index]
+
+		x_filepath = f"{os.path.join(self.root_dir, self.x_folder)}/{self.x_filename_format.replace('*', file_id)}"
+		x = imread(x_filepath)
+		x = torch.tensor(x).permute(2,0,1)
+		
+		if self.y_folder:
+			y_filepath = f"{os.path.join(self.root_dir, self.y_folder)}/{self.y_filename_format.replace('*', file_id)}"
+			y = imread(y_filepath)
+			if len(y.shape) < 3: 
+				y = torch.tensor(y).unsqueeze(-1).permute(2,0,1)
+			else: 
+				y = torch.tensor(y).permute(2,0,1)
+			sample = {'x': x, 'y': y}
+		else:
+			sample = {'x': x}
+
+		if self.transform:
+			sample = self.transform(sample)
+
+		return sample
--- src/transforms.py
+++ src/transforms.py
@@ -1,24 +1,25 @@
 
 import torch
+from torch.utils.data import transform
+import numpy as np
+
 
 
 class Rescale(object):
     """Rescale the image in a sample to a given size.
-
     Args:
         output_size (tuple or int): Desired output size. If tuple, output is
             matched to output_size. If int, smaller of image edges is matched
             to output_size keeping aspect ratio the same.
     """
-
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
         self.output_size = output_size
 
     def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
+        x, y = sample['x'], sample['y']
 
-        h, w = image.shape[:2]
+        h, w = x.shape[:2]
         if isinstance(self.output_size, int):
             if h > w:
                 new_h, new_w = self.output_size * h / w, self.output_size
@@ -29,23 +30,18 @@ class Rescale(object):
 
         new_h, new_w = int(new_h), int(new_w)
 
-        img = transform.resize(image, (new_h, new_w))
+        img = transform.resize(x, (new_h, new_w))
+        msk = transform.resize(x, (new_h, new_w))
 
-        # h and w are swapped for landmarks because for images,
-        # x and y axes are axis 1 and 0 respectively
-        landmarks = landmarks * [new_w / w, new_h / h]
-
-        return {'image': img, 'landmarks': landmarks}
-
+        return {'x': img, 'y': msk}
+    
 
 class RandomCrop(object):
     """Crop randomly the image in a sample.
-
     Args:
         output_size (tuple or int): Desired output size. If int, square crop
             is made.
     """
-
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
         if isinstance(output_size, int):
@@ -55,31 +51,29 @@ class RandomCrop(object):
             self.output_size = output_size
 
     def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
+        x, y = sample['x'], sample['y']
 
-        h, w = image.shape[:2]
+        h, w = x.shape[:2]
         new_h, new_w = self.output_size
 
         top = np.random.randint(0, h - new_h)
         left = np.random.randint(0, w - new_w)
 
-        image = image[top: top + new_h,
-                      left: left + new_w]
+        x = x[top: top + new_h, left: left + new_w]
+        y = y[top: top + new_h, left: left + new_w]
 
-        landmarks = landmarks - [left, top]
-
-        return {'image': image, 'landmarks': landmarks}
+        return {'x': x, 'y': y}
 
 
 class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
+        x, y = sample['x'], sample['y']
 
         # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C x H x W
-        image = image.transpose((2, 0, 1))
-        return {'image': torch.from_numpy(image),
-                'landmarks': torch.from_numpy(landmarks)}
+        # numpy x: H x W x C
+        # torch x: C x H x W
+        x = x.transpose((2, 0, 1))
+        return {'x': torch.from_numpy(x),
+                'y': torch.from_numpy(y)}
